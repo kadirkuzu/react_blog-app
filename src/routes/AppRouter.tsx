6fdba1a16c4b5590a7458715c3127874625161dd
kadirkuzu <kadir.kuzu.5656@gmail.com>
2025-04-16 00:31:03 +0300

    private route

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -4,6 +4,7 @@ import Register from '../pages/auth/register/Page';
 import Layout from '../pages/layout/Layout';
 import BlogList from '../pages/blogs/list/Page';
 import CreateBlog from '../pages/blogs/create/Page';
+import PrivateRoute from './PrivateRoute';
 
 const AppRouter = () => {
   return (
@@ -12,9 +13,13 @@ const AppRouter = () => {
         <Route element={<Layout />}>
           <Route path="/" element={<Navigate to="/blogs" />} />
           <Route path="/blogs" element={<BlogList />} />
-          <Route path="/create" element={<CreateBlog />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route element={<PrivateRoute shouldLoggedIn={true} navigateTo='/login' />}>
+            <Route path="/create" element={<CreateBlog />} />
+          </Route>
+          <Route element={<PrivateRoute shouldLoggedIn={false} navigateTo='/' />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
---
+++ src/routes/PrivateRoute.tsx
@@ -0,0 +1,11 @@
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
+import { RootState } from '../state';
+
+const PrivateRoute = ({shouldLoggedIn,navigateTo}: {shouldLoggedIn: boolean, navigateTo: string}) => {
+  const isAuthenticated = useSelector((state: RootState) => !!state.auth.user == shouldLoggedIn);
+
+  return isAuthenticated ? <Outlet /> : <Navigate to={navigateTo} replace />;
+};
+
+export default PrivateRoute;
